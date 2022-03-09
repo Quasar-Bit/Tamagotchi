b028ef13928e7219aa827b088c50263908e75277
Vladimir <vladimircuraciov@gmail.com>
2022-03-09 02:55:54 +0200

    Changed path to the remote database, changed Animals View

---
+++ Tamagotchi/TamagotchiWeb/Controllers/AnimalsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TamagotchiWeb.Data;
+using TamagotchiWeb.Entities;
+
+namespace TamagotchiWeb.Controllers
+{
+    public class AnimalsController : Controller
+    {
+        private readonly Context _db;
+        public AnimalsController(Context db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Animal> animals = _db.Animals;
+            return View(animals);
+        }
+    }
+}
