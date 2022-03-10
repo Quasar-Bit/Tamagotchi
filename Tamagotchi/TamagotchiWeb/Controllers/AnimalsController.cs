f982a61270195cf9e7c5777ca18779d613ff5ea6
Vladimir <vladimircuraciov@gmail.com>
2022-03-10 03:12:46 +0200

    ready

--- Tamagotchi/TamagotchiWeb/Controllers/AnimalsController.cs
+++ Tamagotchi/TamagotchiWeb/Controllers/AnimalsController.cs
@@ -27,27 +27,21 @@ namespace TamagotchiWeb.Controllers
         [HttpPost]
         public async Task<IActionResult> Synch(string gg)
         {
-            //var firstRequest = await _animalService.GetAnimals(1);
+            var firstRequest = await _animalService.GetAnimals(1);
 
-            //for (int i = 1; i <= firstRequest.Pagination.total_pages; i++)
-            //{
-            //var answer = await _animalService.GetAnimals(1);
-
-            //foreach (var item in answer.Animals)
-            //{
-            //    _db.Add(item);
-            //    //_db.SavedChanges();
-            //}
-            //}
-
-            var result = new GetAnimals
+            for (int i = 1; i <= firstRequest.Pagination.total_pages; i++)
             {
-                Animals = _db.Animals
-            };
+                var answer = await _animalService.GetAnimals(800);
 
-            await Task.Delay(1000);
+                foreach (var item in answer.Animals)
+                {
+                    _db.Animals.Add(item);
+                }
 
-            return View(result);
+                _db.SaveChanges();
+            }
+
+            return RedirectToAction("index");
         }
     }
 }
